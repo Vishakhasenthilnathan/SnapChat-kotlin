323294183963ce57175942c8609a2a2d804de719
Vishakhasenthilnathan <Vishakhasenthilnathan@gmail.com>
2022-10-10 19:13:34 +0530

    Added comments

---
+++ app/src/main/java/com/project/snapchat/ViewSnapsActivity.kt
@@ -0,0 +1,11 @@
+package com.project.snapchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewSnapsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_snaps)
+    }
+}
